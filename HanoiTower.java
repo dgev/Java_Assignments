4d67028d8b6e11eedbfc22e154e540be01810931
dgev <49248374+dgev@users.noreply.github.com>
2019-10-31 22:28:37 +0400

    homework_6

--- HanoiTower.java
+++ HanoiTower.java
@@ -1,28 +1,46 @@
 package am.homework6;
 
+import java.util.Stack;
+
 public class HanoiTower {
 
 	public static void solveHanowiTower(int numberOfDisks) {
 		long startTime = System.nanoTime();
-		recursiveSolver("1", "2", "3", numberOfDisks);
+		Stack<Integer> firstRod = new Stack<Integer>();
+		Stack<Integer> secondRod = new Stack<Integer>();
+		Stack<Integer> thirdRod = new Stack<Integer>();
+		for (int i = numberOfDisks; i > 0; i--) {
+			firstRod.push(i);
+		}
+		System.out.println(recursiveSolver(firstRod, secondRod, thirdRod, numberOfDisks));
 		long estimatedTime = System.nanoTime() - startTime;
 		System.out.println(estimatedTime);
+
 	}
 
-	public static void recursiveSolver(String firstRod, String secondRod, String thirdRod, int numberOfDisks) {
+
+	public static Stack<Integer> recursiveSolver(Stack<Integer> firstRod, Stack<Integer> secondRod,
+			Stack<Integer> thirdRod, int numberOfDisks) {
 		if (numberOfDisks == 1) {
-			System.out.println("Put disk 1 into the rod number " + thirdRod);
+			if (firstRod.contains(1)) {
+				thirdRod.push(firstRod.pop());
+			} else {
+				thirdRod.push(secondRod.pop());
+			}
 		} else {
 			recursiveSolver(firstRod, thirdRod, secondRod, numberOfDisks - 1);
-			System.out.println("Put disk " + numberOfDisks + " into the rod number " + thirdRod);
+			if (firstRod.contains(numberOfDisks)) {
+				thirdRod.push(firstRod.pop());
+			} else {
+				thirdRod.push(secondRod.pop());
+			}
 			recursiveSolver(secondRod, firstRod, thirdRod, numberOfDisks - 1);
-
 		}
-
+		return thirdRod;
 	}
 
 	public static void main(String args[]) {
-		solveHanowiTower(5);
+		solveHanowiTower(100);
 
 	}
 
