8d62a5b626042a82ebf06413125ab1281b21c78a
dgev <49248374+dgev@users.noreply.github.com>
2019-12-16 02:49:28 +0400

    Delete Usd.java

--- src/Usd.java
+++
@@ -1,7 +0,0 @@
-public class Usd implements Currency {
-    public static double rate = 1;
-    public static double converter(String from, double money){
-        return Currency.converter(from, money, rate);
-    }
-}
-
