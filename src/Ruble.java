0d91263e5a737edba32eba685dd98c68f2864138
dgev <49248374+dgev@users.noreply.github.com>
2019-12-16 02:49:18 +0400

    Delete Ruble.java

--- src/Ruble.java
+++
@@ -1,6 +0,0 @@
-public class Ruble implements Currency {
-    public static double rate = 0.016;
-    public static double converter(String from, double money){
-        return Currency.converter(from, money, rate);
-    }
-}
