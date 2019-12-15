9d16342a8e1ee08cd450e8998702a770dde9cf91
dgev <49248374+dgev@users.noreply.github.com>
2019-12-16 02:49:08 +0400

    Delete Pound.java

--- src/Pound.java
+++
@@ -1,6 +0,0 @@
-public class Pound implements Currency {
-    public static double rate = 1.33;
-    public static double converter(String from, double money){
-        return Currency.converter(from, money, rate);
-    }
-}
